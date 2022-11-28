a8957c3d29756256ae1fb1fba0b6a4ba08450672
Lucas Russo <lerwys@gmail.com>
2022-11-28 12:41:48 -0800

    software: use complex conjugate LO table

--- software/src/createLocalOscillatorTables.py
+++ software/src/createLocalOscillatorTables.py
@@ -15,7 +15,7 @@ def quantize(x):
     if (not args.integerOutput): xi /= args.scaleFactor
     return xi
 
-def rfTable(name, Frf, refDivider, refMultiplier, sampleCount, Feff = None):
+def rfTable(name, Frf, refDivider, refMultiplier, sampleCount, Feff = None, conjugate = False):
     Frf = float(Frf)
     Fadc = Frf / refDivider * refMultiplier
     if (not Feff): Feff = Frf - math.floor(Frf / Fadc) * Fadc
@@ -27,10 +27,11 @@ def rfTable(name, Frf, refDivider, refMultiplier, sampleCount, Feff = None):
         Tsamp = 1.0 / Fadc
         for i in range(sampleCount):
             s = quantize(math.sin(w*Tsamp*i))
+            if conjugate: s = -s
             c = quantize(math.cos(w*Tsamp*i))
             f.write("%9.6f,%9.6f\n" % (c, s))
 
-def ptTable(name, Foffset, Frf, refDivider, refMultiplier, sampleCount, Feff = None):
+def ptTable(name, Foffset, Frf, refDivider, refMultiplier, sampleCount, Feff = None, conjugate = False):
     Frf = float(Frf)
     Fadc = Frf / refDivider * refMultiplier
     if (not Feff): Feff = Frf - math.floor(Frf / Fadc) * Fadc
@@ -44,14 +45,17 @@ def ptTable(name, Foffset, Frf, refDivider, refMultiplier, sampleCount, Feff = N
         Tsamp = 1.0 / Fadc
         for i in range(sampleCount):
             sl = quantize(math.sin(wl*Tsamp*i))
+            if conjugate: sl = -sl
             cl = quantize(math.cos(wl*Tsamp*i))
             sh = quantize(math.sin(wh*Tsamp*i))
+            if conjugate: sh = -sh
             ch = quantize(math.cos(wh*Tsamp*i))
             f.write("%9.6f,%9.6f,%9.6f,%9.6f\n" % (cl, sl, ch, sh))
 
 rfTable('rfTableSR.csv', 500, 328,  77, 77)
 rfTable('rfTableSR_81_328.csv', 499.64, 328,  81, 81)
 rfTable('rfTableSR_81_328_bin_20.csv', 499.64, 328,  81, 81, Feff = 499.64/328*20)
+rfTable('rfTableSR_81_328_bin_20_conjugate.csv', 499.64, 328,  81, 81, Feff = 499.64/328*20, conjugate = True)
 rfTable('rfTableSR_80_333.csv', 499.50, 333,  80, 80)
 rfTable('rfTableTL.csv', 500, 328,  77, 77)
 rfTable('rfTableBR.csv', 500, 500, 116, 29)
@@ -61,6 +65,7 @@ ptTable('ptTableSR_81_328_1_2.csv', (499.64/328.0)*(1.0/2.0), 499.64, 328, 81, 8
 ptTable('ptTableSR_80_333_1_2.csv', (499.50/333.0)*(1.0/2.0), 499.50, 333, 80, 80*2)
 ptTable('ptTableSR_81_328_11_19.csv', (499.64/328.0)*(11.0/19.0), 499.64, 328, 81, 81*19)
 ptTable('ptTableSR_81_328_11_19_bin_20.csv', (499.64/328.0)*(11.0/19.0), 499.64, 328, 81, 81*19, Feff = 499.64/328*20)
+ptTable('ptTableSR_81_328_11_19_bin_20_conjugate.csv', (499.64/328.0)*(11.0/19.0), 499.64, 328, 81, 81*19, Feff = 499.64/328*20, conjugate = True)
 ptTable('ptTableSR_11_19.csv', (500.0/328.0)*(11.0/19.0), 500, 328, 77, 77*19)
 ptTable('ptTableTL.csv', 0, 500, 328,  77,  77)
 ptTable('ptTableBR_1_2.csv', (500.0/125.0)*(1.0/2.0), 500, 500, 116,  29*2)
