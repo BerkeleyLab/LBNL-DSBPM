34453b63c5a4fdeb8ccb44cd31c0eea8334c4dcd
Lucas Russo <lerwys@gmail.com>
2024-03-04 15:35:53 -0800

    software: use mdB and single LE for AFE SPI

--- software/src/afe.c
+++ software/src/afe.c
@@ -9,20 +9,23 @@
 #include "afe.h"
 #include "gpio.h"
 #include "rfdc.h"
+#include "systemParameters.h"
 #include "util.h"
 
-#define AFE_CHANNEL_COUNT   8
+#define SPI_DEVSEL_SHIFT    24
+#define SPI_DEVSEL_MASK     0x07000000
 
-#define SPI_DEVSEL_SHIFT 24
-#define SPI_W_24_BIT_OP  0x80000000
-#define SPI_R_BUSY       0x80000000
-#define SPI_DEVSEL_MASK  0x07000000
+#define SPI_W_24_BIT_OP     0x80000000
+#define SPI_W_LSB_FIRST     0x40000000
 
-#define SPI_WRITE(v)    GPIO_WRITE(GPIO_IDX_AFE_SPI_CSR, (v))
-#define SPI_READ()      GPIO_READ(GPIO_IDX_AFE_SPI_CSR)
+#define SPI_R_BUSY          0x80000000
+
+#define REG(base,chan)  ((base) + (GPIO_IDX_PER_DSBPM * (chan)))
 
 #define ADC_RANGE_CSR_CMD_LATCH 0x1
 #define ADC_RANGE_CSR_CMD_SHIFT 0x2
+
+static unsigned int afeAttenuation[CFG_DSBPM_COUNT][CFG_ADC_PER_BPM_COUNT];
 
 void
 afeInit(void)
@@ -33,6 +36,83 @@ int
 afeGetSerialNumber(void)
 {
     return 0;
+}
+
+/*
+ * Set attenuators
+ */
+void
+afeAttenSet(unsigned int bpm, unsigned int channel,
+        unsigned int mdB)
+{
+    static const int address = 0x7;
+    int pass = 0;
+
+    if (bpm >= CFG_DSBPM_COUNT) return;
+    if (channel >= CFG_ADC_PER_BPM_COUNT) return;
+    // All channels are wired together
+    channel = 0;
+
+    /*
+     * Write trimmed value
+     */
+    int v = (mdB * 4)/1000;
+    if (v > 127) v = 127;
+    else if (v < 0) v = 0;
+    // SPI data: 8-bit address | 8-bit data
+    v |= address << 8;
+    // SPI module options
+    v |= SPI_W_LSB_FIRST | ((channel << SPI_DEVSEL_SHIFT) & SPI_DEVSEL_MASK);
+    while  (GPIO_READ(REG(GPIO_IDX_AFE_SPI_CSR, bpm)) & SPI_R_BUSY) {
+        if (++pass >= 100) {
+            warn("ATTENUATOR UPDATE FAILED TO COMPLETE");
+            break;
+        }
+    }
+    GPIO_WRITE(REG(GPIO_IDX_AFE_SPI_CSR, bpm), v);
+
+#if 0
+    /*
+     * Write trimmed value
+     */
+    do {
+        int pass = 0;
+        int v = (mdB * 4)/1000 + systemParameters.afeTrim[i];
+        if (v > 127) v = 127;
+        else if (v < 0) v = 0;
+        // SPI data: 8-bit address | 8-bit data
+        v |= address << 8;
+        // SPI module options
+        v |= SPI_W_LSB_FIRST | ((channel << SPI_DEVSEL_SHIFT) & SPI_DEVSEL_MASK);
+        while  (GPIO_READ(REG(GPIO_IDX_AFE_SPI_CSR, bpm)) & SPI_R_BUSY) {
+            if (++pass >= 100) {
+                warn("ATTENUATOR UPDATE FAILED TO COMPLETE");
+                break;
+            }
+        }
+        GPIO_WRITE(REG(GPIO_IDX_AFE_SPI_CSR, bpm), v);
+    } while (++i < sizeof systemParameters.afeTrim /
+                                            sizeof systemParameters.afeTrim[0]);
+#endif
+
+    /*
+     * Update attenuator compenstation coefficients in FPGA
+     */
+    afeAttenuation[bpm][channel] = mdB;
+}
+
+/*
+ * Read back attenuator setting
+ */
+unsigned int
+afeAttenGet(unsigned int bpm, unsigned int channel)
+{
+    if (bpm >= CFG_DSBPM_COUNT) return 0;
+    if (channel >= CFG_ADC_PER_BPM_COUNT) return 0;
+    // All channels are wired together
+    channel = 0;
+
+    return afeAttenuation[bpm][channel];
 }
 
 void
