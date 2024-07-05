9951d54d71f75778bd3b8c3e9b607cf208bae856
Lucas Russo <lerwys@gmail.com>
2024-07-05 16:07:06 -0700

    syn/prop.tcl: disable verbose implementation flags

--- gateware/syn/common_dsbpm_zcu208/prop.tcl
+++ gateware/syn/common_dsbpm_zcu208/prop.tcl
@@ -5,12 +5,9 @@ set syn_prop_dict {
 
 set impl_prop_dict {
     steps.opt_design.is_enabled          {1}
-    steps.opt_design.args.verbose        {1}
     steps.place_design.args.directive    {Explore}
     steps.phys_opt_design.is_enabled     {1}
-    "steps.phys_opt_design.args.more options" {-verbose}
     steps.phys_opt_design.args.directive {AlternateFlowWithRetiming}
-    steps.write_bitstream.args.verbose   {1}
 }
 
 set_property -dict $syn_prop_dict [get_runs synth_1]
