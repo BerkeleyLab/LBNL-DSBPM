1db28c135ac3510f320243e09b9bb3b45fb72602
Lucas Russo <lerwys@gmail.com>
2024-07-25 12:21:19 -0700

    software: add LBL208 software support

--- software/target/dsbpm_vcxo_117_zcu208/dsbpm_vcxo_117_zcu208.mk
+++ software/target/dsbpm_vcxo_117_zcu208/dsbpm_vcxo_117_zcu208.mk
@@ -8,27 +8,14 @@ __HDR_GEN_DSBPM_ZCU208_FILES = \
 HDR_GEN_DSBPM_ZCU208_FILES = $(addprefix $(DSBPM_ZCU208_DIR)/, $(__HDR_GEN_DSBPM_ZCU208_FILES))
 
 __HDR_DSBPM_ZCU208_FILES = \
-	iic.h \
-	gpio.h \
 	config.h
 HDR_DSBPM_ZCU208_FILES = $(addprefix $(DSBPM_ZCU208_DIR)/, $(__HDR_DSBPM_ZCU208_FILES))
 
-__SRC_DSBPM_ZCU208_FILES = \
-	iic.c
-SRC_DSBPM_ZCU208_FILES = $(addprefix $(DSBPM_ZCU208_DIR)/, $(__SRC_DSBPM_ZCU208_FILES))
-
 # For top-level makfile
 HDR_FILES += $(HDR_GEN_DSBPM_ZCU208_FILES)
-SRC_FILES += $(SRC_DSBPM_ZCU208_FILES)
+HDR_FILES += $(HDR_DSBPM_ZCU208_FILES)
 
-%lmk04828B.h: %lmk04828B.tcs
-	sh $(SW_SCRIPTS_DIR)/createRFCLKheader.sh $< > $@
-
-%lmx2594ADC.h: %lmx2594ADC.tcs
-	sh $(SW_SCRIPTS_DIR)/createRFCLKheader.sh $< > $@
-
-%lmx2594DAC.h: %lmx2594DAC.tcs
-	sh $(SW_SCRIPTS_DIR)/createRFCLKheader.sh $< > $@
+include $(DSBPM_ZCU208_DIR)/../common_dsbpm/common_dsbpm.mk
 
 clean::
 	$(RM) -rf $(HDR_GEN_DSBPM_ZCU208_FILES)
