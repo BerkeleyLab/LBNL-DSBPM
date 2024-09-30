2b94ba55b425a73a7b2f6a785da548b7286529e0
Lucas Russo <lerwys@gmail.com>
2024-09-30 10:15:46 -0700

    software/scripts: remove unused tables from script

--- software/scripts/createLocalOscillatorTables.py
+++ software/scripts/createLocalOscillatorTables.py
@@ -7,6 +7,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Create local oscillator lookup tables.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-i', '--integerOutput', action='store_true', help='Show values as integers, not scaled back to [-1,1].')
 parser.add_argument('-s', '--demodScaleFactor', default=0x1FFFF, type=int, help='Double to Integer scale factor for Demodulation tables (full scale value).')
+parser.add_argument('-t', '--ptScaleFactor', default=0x7FFF, type=int, help='Double to Integer scale factor for Pilot Tone tables (full scale value).')
 args = parser.parse_args()
 
 def quantize(x, scaleFactor, integerOutput = False):
@@ -59,43 +60,15 @@ def ptTableGen(name, FoffsetL, FoffsetH, Frf, refDivider, refMultiplier, sampleC
 def ptTable(name, Foffset, Frf, refDivider, refMultiplier, sampleCount, scaleFactor, Feff = None, conjugate = False):
     ptTableGen(name, Foffset, Foffset, Frf, refDivider, refMultiplier, sampleCount, scaleFactor, Feff = None, conjugate = False)
 
-rfTable('rfTableSR.csv', 500, 328,  77, 77,
-        scaleFactor = args.demodScaleFactor)
-rfTable('rfTableSR_81_328.csv', 499.64, 328,  81, 81,
-        scaleFactor = args.demodScaleFactor)
-rfTable('rfTableSR_81_328_bin_20.csv', 499.64, 328,  81, 81,
-        scaleFactor = args.demodScaleFactor, Feff = 499.64/328*20)
 rfTable('rfTableSR_81_328_bin_20_conjugate.csv', 499.64, 328,  81, 81,
         scaleFactor = args.demodScaleFactor, Feff = 499.64/328*20, conjugate = True)
-rfTable('rfTableSR_80_333.csv', 499.50, 333,  80, 80,
-        scaleFactor = args.demodScaleFactor)
-rfTable('rfTableTL.csv', 500, 328,  77, 77,
-        scaleFactor = args.demodScaleFactor)
-rfTable('rfTableBR.csv', 500, 500, 116, 29,
-        scaleFactor = args.demodScaleFactor)
+rfTable('rfTableSR_77_328_bin_20_conjugate.csv', 499.64, 328,  77, 77,
+        scaleFactor = args.demodScaleFactor, Feff = 499.64/328*20, conjugate = True)
 
-ptTable('ptTableSR_1_2.csv', (500.0/328.0)*(1.0/2.0), 500, 328, 77, 77*2,
-        scaleFactor = args.demodScaleFactor)
-ptTable('ptTableSR_81_328_1_2.csv', (499.64/328.0)*(1.0/2.0), 499.64, 328, 81, 81*2,
-        scaleFactor = args.demodScaleFactor)
-ptTable('ptTableSR_80_333_1_2.csv', (499.50/333.0)*(1.0/2.0), 499.50, 333, 80, 80*2,
-        scaleFactor = args.demodScaleFactor)
-ptTable('ptTableSR_81_328_11_19.csv', (499.64/328.0)*(11.0/19.0), 499.64, 328, 81, 81*19,
-        scaleFactor = args.demodScaleFactor)
-ptTable('ptTableSR_81_328_11_19_bin_20.csv', (499.64/328.0)*(11.0/19.0), 499.64, 328, 81, 81*19,
-        scaleFactor = args.demodScaleFactor, Feff = 499.64/328*20)
 ptTable('ptTableSR_81_328_11_19_bin_20_conjugate.csv', (499.64/328.0)*(11.0/19.0), 499.64, 328, 81, 81*19,
         scaleFactor = args.demodScaleFactor, Feff = 499.64/328*20, conjugate = True)
 ptTable('ptTableSR_81_328_30_19_bin_20_conjugate.csv', (499.64/328.0)*(30.0/19.0), 499.64, 328, 81, 81*19,
         scaleFactor = args.demodScaleFactor, Feff = 499.64/328*20, conjugate = True)
-
 ptTableGen('ptTableSR_81_328_L26_19_H30_19_bin_20_conjugate.csv', (499.64/328.0)*(26.0/19.0),
            (499.64/328.0)*(30.0/19.0), 499.64, 328, 81, 81*19, scaleFactor = args.demodScaleFactor,
            Feff = 499.64/328*20, conjugate = True)
-
-ptTable('ptTableSR_11_19.csv', (500.0/328.0)*(11.0/19.0), 500, 328, 77, 77*19,
-        scaleFactor = args.demodScaleFactor)
-ptTable('ptTableTL.csv', 0, 500, 328,  77,  77,
-        scaleFactor = args.demodScaleFactor)
-ptTable('ptTableBR_1_2.csv', (500.0/125.0)*(1.0/2.0), 500, 500, 116,  29*2,
-        scaleFactor = args.demodScaleFactor)
