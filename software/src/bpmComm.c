ea50ff8f242ab891f0017f9dccfcd6efeb67136c
Lucas Russo <lerwys@gmail.com>
2026-07-22 16:48:19 -0700

    software/*/bpmComm: re-write macros in terms of reg_macros macros

--- software/src/bpmComm.c
+++ software/src/bpmComm.c
@@ -2,6 +2,7 @@
  * BPM/cell controller communication
  */
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <xparameters.h>
@@ -23,9 +24,9 @@ bpmCommSetFOFB(unsigned int bpm, int fofbIndex)
     if ((fofbIndex >= 0) && (fofbIndex < DSBPM_PROTOCOL_FOFB_CAPACITY))  {
         reg &= ~BPMCOMM_CSR_FOFB_INDEX_MASK;
         reg |= BPMCOMM_CSR_FOFB_INDEX_VALID |
-            ((fofbIndex << BPMCOMM_CSR_FOFB_INDEX_SHIFT) &
-             BPMCOMM_CSR_FOFB_INDEX_MASK);
+            BPMCOMM_CSR_FOFB_INDEX_W(fofbIndex);
     }
+
     GPIO_WRITE(REG(GPIO_IDX_CELL_COMM_BPM_CSR, bpm), reg);
 }
 
@@ -34,6 +35,9 @@ bpmCommGetFOFB(unsigned int bpm)
 {
     if (bpm >= CFG_DSBPM_COUNT) return -1;
 
-     return (GPIO_READ(REG(GPIO_IDX_CELL_COMM_BPM_CSR, bpm)) &
-             BPMCOMM_CSR_FOFB_INDEX_MASK) >> BPMCOMM_CSR_FOFB_INDEX_SHIFT;
+    uint32_t reg = GPIO_READ(REG(GPIO_IDX_CELL_COMM_BPM_CSR, bpm));
+
+    return BPMCOMM_CSR_FOFB_INDEX_R(reg);
+}
+
 }
--- software/src/bpmComm.h
+++ software/src/bpmComm.h
@@ -5,10 +5,28 @@
 #ifndef _BPMCOMM_H_
 #define _BPMCOMM_H_
 
-#define BPMCOMM_CSR_FOFB_INDEX_MASK    0x1FF0000
-#define BPMCOMM_CSR_FOFB_INDEX_SHIFT   16
-#define BPMCOMM_CSR_FOFB_INDEX_VALID   0x2000000
-#define BPMCOMM_CSR_FOFB_INDEX_ENABLE  0x4000000
+#include "reg_macros.h"
+
+#define BPMCOMM_CSR_FOFB_INDEX_SIZE                 9
+#define BPMCOMM_CSR_FOFB_INDEX_SHIFT                16
+#define BPMCOMM_CSR_FOFB_INDEX_MASK                 REG_GEN_MASK(BPMCOMM_CSR_FOFB_INDEX_SHIFT, \
+                                                            BPMCOMM_CSR_FOFB_INDEX_SIZE)
+#define BPMCOMM_CSR_FOFB_INDEX_R(reg)               REG_GEN_READ(reg, BPMCOMM_CSR_FOFB_INDEX_SHIFT, \
+                                                            BPMCOMM_CSR_FOFB_INDEX_SIZE)
+#define BPMCOMM_CSR_FOFB_INDEX_W(value)             REG_GEN_WRITE(value, BPMCOMM_CSR_FOFB_INDEX_SHIFT, \
+                                                            BPMCOMM_CSR_FOFB_INDEX_SIZE)
+
+#define BPMCOMM_CSR_FOFB_INDEX_VALID_SIZE           1
+#define BPMCOMM_CSR_FOFB_INDEX_VALID_SHIFT          25
+#define BPMCOMM_CSR_FOFB_INDEX_VALID_MASK           REG_GEN_MASK(BPMCOMM_CSR_FOFB_INDEX_VALID_SHIFT, \
+                                                      BPMCOMM_CSR_FOFB_INDEX_VALID_SIZE)
+#define BPMCOMM_CSR_FOFB_INDEX_VALID                BPMCOMM_CSR_FOFB_INDEX_VALID_MASK
+
+#define BPMCOMM_CSR_FOFB_INDEX_ENABLE_SIZE          1
+#define BPMCOMM_CSR_FOFB_INDEX_ENABLE_SHIFT         26
+#define BPMCOMM_CSR_FOFB_INDEX_ENABLE_MASK          REG_GEN_MASK(BPMCOMM_CSR_FOFB_INDEX_ENABLE_SHIFT, \
+                                                     BPMCOMM_CSR_FOFB_INDEX_ENABLE_SIZE)
+#define BPMCOMM_CSR_FOFB_INDEX_ENABLE               BPMCOMM_CSR_FOFB_INDEX_ENABLE_MASK
 
 void bpmCommSetFOFB(unsigned int bpm, int fofbIndex);
 int bpmCommGetFOFB(unsigned int bpm);
